d11536e197d52881765f583754228c1d2f815cdc
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2020-04-06 00:36:10 +0900

    Use forwardRef to get ref of tui.editor

--- components/WysiwygEditor.tsx
+++ components/WysiwygEditor.tsx
@@ -1,17 +1,50 @@
 import dynamic from 'next/dynamic';
 import * as React from 'react';
-import { EditorProps } from '@toast-ui/react-editor';
+import { Editor as EditorType, EditorProps } from '@toast-ui/react-editor';
+import { TuiEditorWithForwardedProps } from './TuiEditorWrapper';
 
-const Editor = dynamic<EditorProps>(() => import('@toast-ui/react-editor').then(m => m.Editor), { ssr: false });
-
-const WysiwygEditor: React.FC = () => {
-  return <Editor
-    initialValue="hello react editor world!"
-    previewStyle="vertical"
-    height="600px"
-    initialEditType="markdown"
-    useCommandShortcut={true}
-  />;
+interface EditorPropsWithHandlers extends EditorProps {
+  onChange?(value: string): void;
 }
 
+const Editor = dynamic<TuiEditorWithForwardedProps>(() => import("./TuiEditorWrapper"), { ssr: false });
+const EditorWithForwardedRef = React.forwardRef<EditorType | undefined, EditorPropsWithHandlers>((props, ref) => (
+  <Editor {...props} forwardedRef={ref as React.MutableRefObject<EditorType>} />
+));
+
+interface Props extends EditorProps {
+  onChange(value: string): void;
+
+  valueType?: "markdown" | "html";
+}
+
+const WysiwygEditor: React.FC<Props> = (props) => {
+  const { initialValue, previewStyle, height, initialEditType, useCommandShortcut } = props;
+
+  const editorRef = React.useRef<EditorType>();
+  const handleChange = React.useCallback(() => {
+    if (!editorRef.current) {
+      return;
+    }
+
+    const instance = editorRef.current.getInstance();
+    const valueType = props.valueType || "markdown";
+
+    props.onChange(valueType === "markdown" ? instance.getMarkdown() : instance.getHtml());
+  }, [props, editorRef]);
+
+  return <div>
+    <EditorWithForwardedRef
+      {...props}
+      initialValue={initialValue || "hello react editor world!"}
+      previewStyle={previewStyle || "vertical"}
+      height={height || "600px"}
+      initialEditType={initialEditType || "markdown"}
+      useCommandShortcut={useCommandShortcut || true}
+      ref={editorRef}
+      onChange={handleChange}
+    />
+  </div>;
+};
+ 
 export default WysiwygEditor;
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const IndexPage = () => (
         <a>About</a>
       </Link>
     </p>
-    <WysiwygEditor />
+    <WysiwygEditor onChange={(value) => console.log(value)} />
   </Layout>
 )
 
